7555315858f8f2777895baa8d51b5271df33f573
Yu Hongchao <2871314919@qq.com>
2018-04-23 15:19:15 +0800

    初次提交

--- main/src/nlp.py
+++ main/src/nlp.py
@@ -3,3 +3,11 @@
 @Time:2018/4/20 20:06
 @Author:yuhongchao
 """
+#这个是文本多样性
+
+def lexical_diversity(text):
+    return len(text) / len(set(text))
+
+def precentage(count , total):
+    return 100*count/total
+
